839a9ba83f37655f1416ceb9ee9636f21b85420b
awilsm <awilsonmyers@gmail.com>
2015-11-12 21:50:41 -0500

    Add share button functionality to NewsActivity

--- invest-android/Invest/app/src/main/java/com/investmobile/invest/NewsActivity.java
+++ invest-android/Invest/app/src/main/java/com/investmobile/invest/NewsActivity.java
@@ -3,22 +3,27 @@ package com.investmobile.invest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 public class NewsActivity extends AppCompatActivity {
+    Intent intent;
+    String title;
+    String link;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = getIntent();
-        String title = intent.getStringExtra("title");
-        String link = intent.getStringExtra("link");
+        intent = getIntent();
+        title = intent.getStringExtra("title");
+        link = intent.getStringExtra("link");
         setContentView(R.layout.activity_news);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
         TextView textView = (TextView) findViewById(R.id.newsTitle);
         textView.setLines(1);
         textView.setText(title);
@@ -34,5 +39,33 @@ public class NewsActivity extends AppCompatActivity {
             view.loadUrl(url);
             return true;
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_news, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_share:
+                Intent sendIntent = new Intent(Intent.ACTION_SEND);
+                sendIntent.setType("text/plain");
+                sendIntent.putExtra(Intent.EXTRA_TEXT, link);
+                sendIntent.putExtra(Intent.EXTRA_SUBJECT, title);
+                sendIntent = Intent.createChooser(sendIntent, "Send stock news via");
+                startActivity(sendIntent);
+                return true;
+            case R.id.action_settings:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
